0961584c62adf1127a86248f844c06c568f7e9de
alicorak0 <ali_123-fb@hotmail.com>
2026-02-05 22:21:12 +0300

    5 Şubat 22

--- src/app/admin/pages/admin-panel-component/admin-panel-component.ts
+++ src/app/admin/pages/admin-panel-component/admin-panel-component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+// import { UploadPhotoService } from '../../../services/upload-photo-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-panel-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-panel-component.html',
   styleUrl: './admin-panel-component.css',
 })
 export class AdminPanelComponent {
 
+ 
 }
