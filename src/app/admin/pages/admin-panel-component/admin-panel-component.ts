eef94ada21caff0abbda895ff51133d942f66274
birayaltun <149802729+birayaltun@users.noreply.github.com>
2026-02-05 23:30:12 +0300

    5 Şubat 23

--- src/app/admin/pages/admin-panel-component/admin-panel-component.ts
+++ src/app/admin/pages/admin-panel-component/admin-panel-component.ts
@@ -4,11 +4,20 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-panel-component',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './admin-panel-component.html',
   styleUrl: './admin-panel-component.css',
 })
 export class AdminPanelComponent {
 
- 
+  isSidebarOpen = false;
+
+  openSidebar() {
+    this.isSidebarOpen = true;
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen = false;
+  }
 }
---
+++ src/app/admin/pages/categoryaddcomponent/categoryaddcomponent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoryaddcomponent',
+  imports: [],
+  templateUrl: './categoryaddcomponent.html',
+  styleUrl: './categoryaddcomponent.css',
+})
+export class Categoryaddcomponent {
+
+}
